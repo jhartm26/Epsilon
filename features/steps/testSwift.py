5f86484ea8d494ade6aca3866d11752af0a1513d
jhartm26 <90359108+jhartm26@users.noreply.github.com>
2021-11-29 16:40:18 -0500

    Test Changes

--- features/steps/testSwift.py
+++ features/steps/testSwift.py
@@ -10,7 +10,7 @@ def step_impl(context, url):
     browser = webdriver.Chrome()
     browser.get(url)
     context.browser = browser
-    #time.sleep(200)
+    time.sleep(200)
 
 @when('name and date for task is written')
 def step_impl(context):
@@ -27,20 +27,6 @@ def step_impl(context):
 def step_impl(context):
     tomorrowTaskList = list(context.browser.find_elements_by_id('task-list-2021-11-02'))
     assert(len(tomorrowTaskList) > 0)
-
-@when('date to find is written')
-def step_impl(context):
-    taskDate_element = context.browser.find_element_by_id('input-date-find')
-    taskDate_element.clear()
-    taskDate_element.send_keys('11022021')
-    taskDate_element.send_keys(Keys.RETURN)
-    context.browser.find_element_by_id('save_edit-date-find').click()
-
-@then('we open that day')
-def step_impl(context):
-    currentDateEle = context.browser.find_element_by_id('curr_date')
-    currentDate = currentDateEle.text
-    assert(currentDate == '11/2/2021')
 
 #################################################################################
 
